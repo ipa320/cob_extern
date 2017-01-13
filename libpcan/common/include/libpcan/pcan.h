f06ffa5cc3a90444efe94561637fd974a20a44bd
Benjamin Maidel <bnm@ipa.fhg.de>
2017-01-13 14:50:35 +0100

    update to peak driver 8.3

--- libpcan/common/include/libpcan/pcan.h
+++ libpcan/common/include/libpcan/pcan.h
@@ -4,7 +4,7 @@
 //****************************************************************************
 // Copyright (C) 2001-2010  PEAK System-Technik GmbH
 //
-// linux@peak-system.com 
+// linux@peak-system.com
 // www.peak-system.com
 //
 // This program is free software; you can redistribute it and/or modify
@@ -27,9 +27,9 @@
 //****************************************************************************
 //
 // pcan.h
-// constants and definitions to access the drivers 
+// constants and definitions to access the drivers
 //
-// $Id: pcan.h 615 2010-02-14 22:38:55Z khitschler $
+// $Id: pcan.h 1376 2016-11-22 16:02:22Z stephane $
 //
 //****************************************************************************
 
@@ -58,17 +58,21 @@
 #endif
 
 //****************************************************************************
-// parameter wHardwareType, used by open 
-#define HW_ISA             1 // not supported with LINUX, 82C200 chip 
+// parameter wHardwareType, used by open
+#define HW_ISA             1 // not supported with LINUX, 82C200 chip
 #define HW_DONGLE_SJA      5
-#define HW_DONGLE_SJA_EPP  6 
-#define HW_DONGLE_PRO      7 // not yet supported with LINUX
-#define HW_DONGLE_PRO_EPP  8 // not yet supported with LINUX
+#define HW_DONGLE_SJA_EPP  6
+#define HW_DONGLE_PRO      7
+#define HW_DONGLE_PRO_EPP  8
 #define HW_ISA_SJA         9 // use this also for PC/104
-#define HW_PCI            10 // PCI carries always SJA1000 chips
+#define HW_PCI            10 // this kind of PCI carries SJA1000 chips
 #define HW_USB            11 // don't know if this is common over peak products
 #define HW_PCCARD         12 // not aligned to other OS
-#define HW_USB_PRO        13 
+#define HW_USB_PRO        13
+#define HW_USB_PRO_FD     17	/* same as Device ID (why not?) */
+#define HW_USB_FD         18	/* same as Device ID (why not?) */
+#define HW_PCI_FD         19	/* this kind of PCI runs uCAN FPGA */
+#define HW_USB_X6         20	/* same as Device ID (why not?) */
 
 //****************************************************************************
 // mask for standard and extended CAN identifiers
@@ -85,7 +89,7 @@
 #define CAN_ERR_BUSOFF         0x0010  // bus error, 'bus off' state entered
 #define CAN_ERR_QRCVEMPTY      0x0020  // receive queue is empty
 #define CAN_ERR_QOVERRUN       0x0040  // receive queue overrun
-#define CAN_ERR_QXMTFULL       0x0080  // transmit queue full 
+#define CAN_ERR_QXMTFULL       0x0080  // transmit queue full
 #define CAN_ERR_REGTEST        0x0100  // test of controller registers failed
 #define CAN_ERR_NOVXD          0x0200  // Win95/98/ME only
 #define CAN_ERR_RESOURCE       0x2000  // can't create resource
@@ -95,96 +99,86 @@
 
 //****************************************************************************
 // MSGTYPE bits of element MSGTYPE in structure TPCANMsg
-#define MSGTYPE_STATUS        0x80     // used to mark a status TPCANMsg
-#define MSGTYPE_EXTENDED      0x02     // declares a extended frame
-#define MSGTYPE_RTR           0x01     // marks a remote frame
 #define MSGTYPE_STANDARD      0x00     // marks a standard frame
+#define MSGTYPE_RTR           0x01     // marks a remote frame
+#define MSGTYPE_EXTENDED      0x02     // declares a extended frame
+#define MSGTYPE_SELFRECEIVE   0x04     // self-received message
+#define MSGTYPE_STATUS        0x80     // used to mark a status TPCANMsg
 
 //****************************************************************************
 // maximum length of the version string (attention: used in driver too)
-#define VERSIONSTRING_LEN     64  
+#define VERSIONSTRING_LEN     64
 
 //****************************************************************************
 // structures to communicate via ioctls
-typedef struct
-{
-  WORD wBTR0BTR1;        // merged BTR0 and BTR1 register of the SJA1000
-  BYTE ucCANMsgType;     // 11 or 29 bits - put MSGTYPE_... in here
-  BYTE ucListenOnly;     // listen only mode when != 0
+typedef struct pcan_init {
+	WORD wBTR0BTR1;        // merged BTR0 and BTR1 register of the SJA1000
+	BYTE ucCANMsgType;     // 11 or 29 bits - put MSGTYPE_... in here
+	BYTE ucListenOnly;     // listen only mode when != 0
 } TPCANInit;             // for PCAN_INIT
 
-typedef struct 
-{
-  DWORD ID;              // 11/29 bit code
-  BYTE  MSGTYPE;         // bits of MSGTYPE_*
-  BYTE  LEN;             // count of data bytes (0..8)
-  BYTE  DATA[8];         // data bytes, up to 8
+typedef struct pcan_msg {
+	DWORD ID;              // 11/29 bit code
+	BYTE  MSGTYPE;         // bits of MSGTYPE_*
+	BYTE  LEN;             // count of data bytes (0..8)
+	BYTE  DATA[8];         // data bytes, up to 8
 } TPCANMsg;              // for PCAN_WRITE_MSG
 
-typedef struct
-{
-  TPCANMsg Msg;          // the above message
-  DWORD    dwTime;       // a timestamp in msec, read only
-  WORD     wUsec;        // remainder in micro-seconds
+typedef struct pcan_rd_msg {
+	TPCANMsg Msg;          // the above message
+	DWORD    dwTime;       // a timestamp in msec, read only
+	WORD     wUsec;        // remainder in micro-seconds
 } TPCANRdMsg;            // for PCAN_READ_MSG
 
-typedef struct 
-{
-  WORD  wErrorFlag;      // same as in TPDIAG, is cleared in driver after access
-  int   nLastError;      // is cleared in driver after access
+typedef struct pcan_status {
+	WORD  wErrorFlag;      // same as in TPDIAG, is cleared in driver after access
+	int   nLastError;      // is cleared in driver after access
 } TPSTATUS;              // for PCAN_GET_STATUS
 
-typedef struct
-{
-  WORD  wType;           // the type of interface hardware - see HW_....
-  DWORD dwBase;          // the base address or port of this device
-  WORD  wIrqLevel;       // the irq level of this device
-  DWORD dwReadCounter;   // counts all reads to this device from start
-  DWORD dwWriteCounter;  // counts all writes
-  DWORD dwIRQcounter;    // counts all interrupts
-  DWORD dwErrorCounter;  // counts all errors
-  WORD  wErrorFlag;      // gathers all errors
-  int   nLastError;      // the last local error for this device
-  int   nOpenPaths;      // number of open paths for this device
-  char  szVersionString[VERSIONSTRING_LEN]; // driver version string
+typedef struct pcan_diag {
+	WORD  wType;           // the type of interface hardware - see HW_....
+	DWORD dwBase;          // the base address or port of this device
+	WORD  wIrqLevel;       // the irq level of this device
+	DWORD dwReadCounter;   // counts all reads to this device from start
+	DWORD dwWriteCounter;  // counts all writes
+	DWORD dwIRQcounter;    // counts all interrupts
+	DWORD dwErrorCounter;  // counts all errors
+	WORD  wErrorFlag;      // gathers all errors
+	int   nLastError;      // the last local error for this device
+	int   nOpenPaths;      // number of open paths for this device
+	char  szVersionString[VERSIONSTRING_LEN]; // driver version string
 } TPDIAG;                // for PCAN_DIAG, in opposition to PCAN_GET_STATUS nothing is cleared
-  
-typedef struct
-{
-  DWORD dwBitRate;       // in + out, bitrate in bits per second
-  WORD  wBTR0BTR1;       // out only: the result
+
+typedef struct pcan_btr0btr1 {
+	DWORD dwBitRate;       // in + out, bitrate in bits per second
+	WORD  wBTR0BTR1;       // out only: the result
 } TPBTR0BTR1;
 
-typedef struct 
-{
-  WORD  wErrorFlag;      // same as in TPDIAG, is cleared in driver after access
-  int   nLastError;      // is cleared in driver after access
-  int   nPendingReads;   // count of unread telegrams
-  int   nPendingWrites;  // count of unsent telegrams
+typedef struct pcan_ext_status {
+	WORD  wErrorFlag;      // same as in TPDIAG, is cleared in driver after access
+	int   nLastError;      // is cleared in driver after access
+	int   nPendingReads;   // count of unread telegrams
+	int   nPendingWrites;  // count of unsent telegrams
 } TPEXTENDEDSTATUS;      // for PCAN_GET_ESTATUS
 
-typedef struct
-{
-  DWORD FromID;          // First CAN ID to accept
-  DWORD ToID;            // Last CAN ID to accept
-  BYTE  MSGTYPE;         // bits of MSGTYPE_*
+typedef struct pcan_msg_filter {
+	DWORD FromID;          // First CAN ID to accept
+	DWORD ToID;            // Last CAN ID to accept
+	BYTE  MSGTYPE;         // bits of MSGTYPE_*
 } TPMSGFILTER;
 
 //****************************************************************************
 // currently available sub-functions
 #define SF_GET_SERIALNUMBER 1 // to get the serial number (currently only pcan-usb)
-#define SF_SET_SERIALNUMBER 2 // to set the serial number (currently only pcan-usb)
 #define SF_GET_HCDEVICENO   3 // request hardcoded device number (currently only pcan-usb)
 #define SF_SET_HCDEVICENO   4 // to set hardcoded device number (currently only pcan-usb)
 
-typedef struct
-{
-  int   nSubFunction;    // a sub-function number SF_... to determine the union element used
-  union
-  {
-    DWORD dwSerialNumber; // to get and set the pcan-usb serial number
-    BYTE  ucHCDeviceNo;   // only for USB-devices to get or set a hard assigned number
-  } func;
+typedef struct pcan_extra_params {
+	int   nSubFunction;    // a sub-function number SF_... to determine the union element used
+	union {
+		DWORD dwSerialNumber; // to get the pcan-usb serial number
+		BYTE  ucHCDeviceNo;   // only for USB-devices to get or set a hard assigned number
+	} func;
 } TPEXTRAPARAMS;
 
 //****************************************************************************
